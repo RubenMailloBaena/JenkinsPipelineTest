a9ba1f781b77f0c7e419b4b1d7b5e9fc3fd6025a
RubenMailloBaena <rubenmaillo2003@gmail.com>
2025-06-06 13:08:15 +0200

    Add input XML and converted Java files

---
+++ resources/Java/InputXMLexample2.java
@@ -0,0 +1,53 @@
+public class Animal{
+
+	private String param1 = "Valor1";
+	private boolean param2 = true;
+	private String param3 = "Valor3---";
+	private String result1 = "Result2";
+	private int result2 = 1234;
+
+	public String getParam1(){
+		return param1;
+	}
+
+	public void setParam1(String param1){
+		this.param1 = param1;
+	}
+
+
+	public boolean getParam2(){
+		return param2;
+	}
+
+	public void setParam2(boolean param2){
+		this.param2 = param2;
+	}
+
+
+	public String getParam3(){
+		return param3;
+	}
+
+	public void setParam3(String param3){
+		this.param3 = param3;
+	}
+
+
+	public String getResult1(){
+		return result1;
+	}
+
+	public void setResult1(String result1){
+		this.result1 = result1;
+	}
+
+
+	public int getResult2(){
+		return result2;
+	}
+
+	public void setResult2(int result2){
+		this.result2 = result2;
+	}
+
+}
